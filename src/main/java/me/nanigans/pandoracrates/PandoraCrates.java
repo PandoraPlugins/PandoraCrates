a4f43865a4421f8aeb994eb5d698a78918bb73a7
ProNanigans <pro.nanigans@gmail.com>
2020-12-30 10:57:43 -0500

    Initial commit

---
+++ src/main/java/me/nanigans/pandoracrates/PandoraCrates.java
@@ -0,0 +1,17 @@
+package me.nanigans.pandoracrates;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PandoraCrates extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
